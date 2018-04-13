4e1ba71d87d22c5c64d7b022107e7de4cc10233f
Masanori Funaki <funakimfunaki@gmail.com>
2018-04-13 10:06:26 +0000

    sql設定

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var connection = require('../mysqlConnection');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
 });
+
+
+router.post('/', function (req, res, next) {
+    var title = req.body.title;
+    var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+    var id = 4;
+    var query = `INSERT INTO boards (title, created_at) VALUES ("${title}", "${createdAt}")`;
+    console.log(query);
+    connection.query(query, function (err, rows) {
+        console.log(rows);
+        res.redirect('/');
+    });
+});
+
 
 module.exports = router;
